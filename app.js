803894979fa827c8203baf1ec7137ca44a925e32
vhreva <vitaliyhreva@gmail.com>
2020-04-04 14:33:51 +0300

    app commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const path = require('path')
+const app = express()
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+
+app.get('/', (request, response) => {
+  response.render('home', {
+    name: 'Vitaliy'
+  })
+})
+
+app.use((err, request, response, next) => {
+  console.log(err)
+  response.status(500).send('Something broke!')
+})
+
+app.listen(5000)
